747d97cfa876449bdff6139a7b61d7ac1523cd5f
shaho <toofani@gmail.com>
2019-10-28 15:28:13 +0330

    Add Palindrome algorithms in two different ways

---
+++ algos/Palindrome/method2.js
@@ -0,0 +1,16 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+function palindrome(str) {
+  return str.split("").every((character, index) => {
+    return character === str[str.length - index - 1];
+  });
+}
+
+module.exports = palindrome;
