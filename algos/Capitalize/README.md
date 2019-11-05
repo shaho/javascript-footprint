f78e6ec4493ad6a987a02f275738f2c7843b81d2
shaho <toofani@gmail.com>
2019-11-05 10:00:54 +0330

    Add first approach to Capitalize which is a function that accept a string and capitalize the first letter of each word in the string

---
+++ algos/Capitalize/README.md
@@ -0,0 +1,13 @@
+# Capitalize
+
+A function that accepts a string. The function should capitalize the first letter of each word in the string then return the capitalized string
+
+##### --- Example
+
+`capitalize("a short sentence")` --> 'A Short Sentence'<br />
+`capitalize("a lazy fox")` --> 'A Lazy Fox'<br />
+`capitalize("look, it is working!")` --> 'Look, It Is Working!'
+
+### Read and learn more:
+
+- [AlgoCasts](https://github.com/StephenGrider/AlgoCasts)
---
+++ algos/Capitalize/index.js
@@ -0,0 +1,20 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {
+  const words = [];
+
+  for (let word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return words.join(" ");
+}
+
+module.exports = capitalize;
